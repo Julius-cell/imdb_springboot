8dbc13b3969828701f4fade6b808db9bcf768c41
Julius-cell <julio.cid.b@gmail.com>
2021-01-17 23:31:24 -0300

    Update Model
    Add unique to title

--- src/main/java/cl/example/imdb/entity/Peliculas.java
+++ src/main/java/cl/example/imdb/entity/Peliculas.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -22,7 +23,11 @@ import lombok.Data;
  */
 @Data			// Genera el boilerplate asociado con POJO's (Plain Old Java Object) y Beans.
 @Entity			// Establece que la clase sea una Entidad.
-@Table(name = "Peliculas")
+@Table(name = "Peliculas",
+		uniqueConstraints = {
+				@UniqueConstraint(name = "pelicula_title_unique", columnNames = "title")
+		}
+)
 @ApiModel(description = "Detalles sobre Tabla Peliculas")
 public class Peliculas {
 
